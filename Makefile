2cc450139c0d909353e87b1e23a2b26b415874da
asulon <adrien.sulon@epitech.eu>
2026-03-20 01:40:18 +0100

    Finish parsing and create validation_config to check config data

---
+++ Makefile
@@ -0,0 +1,26 @@
+.PHONY: install run debug clean lint lint-strict
+
+install:
+	pip install -r requirements.txt
+
+run:
+	python a_maze_ing.py config.txt
+
+debug:
+	python -m pdb a_maze_ing.py config.txt
+
+clean:
+	-del /s /q /f *.pyc
+	-rmdir /s /q __pycache__
+	-rmdir /s /q .mypy_cache
+	-rmdir /s /q *.egg-info
+	-rmdir /s /q dist
+	-rmdir /s /q build
+
+lint:
+	flake8 .
+	mypy . --warn-return-any --warn-unused-ignores --ignore-missing-imports --disallow-untyped-defs --check-untyped-defs
+
+lint-strict:
+	flake8 .
+	mypy . --strict
--- a_maze_ing.py
+++ a_maze_ing.py
@@ -3,18 +3,24 @@
 #                                                      :::      ::::::::    #
 #  a_maze_ing.py                                     :+:      :+:    :+:    #
 #                                                  +:+ +:+         +:+      #
-#  By: asulon <asulon@student.42.fr>             +#+  +:+       +#+         #
+#  By: asulon <asulon@student.42nice.fr>         +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 0026/03/08 00:24:33 by sulon           #+#    #+#               #
-#  Updated: 2026/03/19 19:18:50 by asulon          ###   ########.fr        #
+#  Updated: 2026/03/20 01:38:16 by asulon          ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
-from typing import Dict
+from typing import Dict, Any
+import sys
+
+
+class ConfigError(Exception):
+    def __init__(self, message: str) -> None:
+        print(f"Configation Error : {message}")
 
 
 def parse_config(filename: str) -> Dict[str, str]:
-
+    """Parse data from config file return Dict[Key: Value]"""
     config = {}
     try:
         with open(filename) as file:
@@ -22,25 +28,67 @@ def parse_config(filename: str) -> Dict[str, str]:
             for line in config_list:
                 if line.startswith("#"):
                     continue
+                """split Key value"""
                 splited_line = line.split("=")
                 if len(splited_line[0]) > 0:
                     config.update({splited_line[0]: splited_line[1]})
     except (FileNotFoundError, PermissionError) as error:
-        print(f"Error while parsing_config : {error}")
-
+        ConfigError(f"'{error.filename}' not found")
+        sys.exit(1)
+    except ValueError:
+        ConfigError(f"error near '{line}'")
+        sys.exit(1)
     return config
 
 
-def check_config_errors(config: Dict[str, str]) -> bool:
-    print(config)
-    pass
+def validate_config(config: Dict[str, Any]) -> Dict[str, Any]:
+    """Check config file keys and values"""
+
+    """Checking required keys"""
+    required_key = ['WIDTH', 'HEIGHT', 'ENTRY',
+                    'EXIT', 'OUTPUT_FILE', 'PERFECT']
+    for key in required_key:
+        if key not in config:
+            print(
+                f"Error: Missing mandatory key '{key}' in configuration.")
+            sys.exit(1)
+    try:
+        """Assign new values with the correct types to the config keys"""
+        config['WIDTH'] = int(config['WIDTH'])
+        config['HEIGHT'] = int(config['HEIGHT'])
+        config['ENTRY'] = tuple(map(int, config['ENTRY'].split(',')))
+        config['EXIT'] = tuple(map(int, config['EXIT'].split(',')))
+
+        config['PERFECT'] = config['PERFECT'].upper() in [
+            "Y", "TRUE", '1', 'YES']
+    except (ValueError, TypeError):
+        print("Error: Invalid value type in configuration.")
+        sys.exit(1)
+
+    """Testing invalid values"""
+    if config['WIDTH'] < 0 or config['HEIGHT'] < 0:
+        print("Error: WIDTH and HEIGHT must be positive integers.")
+        sys.exit(1)
+    w, h = config['WIDTH'], config['HEIGHT']
+    if not (0 <= config['ENTRY'][0] <= w and 0 <= config['ENTRY'][1] <= h):
+        print("Error : Entry out of bounds")
+    if not (0 <= config['EXIT'][0] <= w and 0 <= config['EXIT'][1] <= h):
+        print("Error : Entry out of bounds")
+    if config['ENTRY'] == config['EXIT']:
+        print("Error: ENTRY and EXIT coordinates cannot be the same.")
+        sys.exit(1)
+    return config
 
 
 def main():
-    config = parse_config("config.txt")
-    check_config_errors(config)
-    # TODO: Get data from config.txt
-    # check valid data
+    if (len(sys.argv) != 2):
+        print(f"Usage: {sys.argv[0]} <config_file>")
+        sys.exit(1)
+    raw_config = parse_config(sys.argv[1])
+    config = validate_config(raw_config)
+    print(config)
+    # TODO: init maze generator
+    # create maze
     pass
 
 
